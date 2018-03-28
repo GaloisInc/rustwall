8b9c9845c795723ed8a2392ec0b1727146e7f12a
Michal Podhradsky <mpodhradsky@galois.com>
2018-03-28 10:31:11 -0700

    Document MTU changes

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# To view/change MTU settings of a device:
+#  ip link show | grep mtu
+#  sudo ip link set tap1 mtu 1500
 sudo ip tuntap add dev tap1 mode tap
 sudo ip addr add 192.168.69.2/24 broadcast 192.168.69.255 dev tap1
 sudo ip link set tap1 up
